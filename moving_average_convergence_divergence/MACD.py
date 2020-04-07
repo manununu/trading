a710188aea4a917b3d394d9eb1036e0a88443a73
manu <manu@mac.local>
2020-04-07 21:14:32 +0200

    fixed bug and added outputfile argument

--- moving_average_convergence_divergence/MACD.py
+++ moving_average_convergence_divergence/MACD.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 import pandas as pd
 from datetime import datetime as dt
-import re
 import argparse
 import os
 from progress.bar import Bar
@@ -14,13 +13,14 @@ def main():
     # Argument parser
     parser = argparse.ArgumentParser(description='Get most profitable moving averages for MACD strategy.') 
     parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='Input csv file with no header and the following columns: date, high, low, close')
-    #parser.add_argument('-o', '--output', default=dir+'/output.csv', help='Specify output file for results. Default: output.csv')
-    parser.add_argument('-m', '--maxema', default=100, help='Specify the maximum EMA')
-    parser.add_argument('-n', '--steps', default=10, help='Specify step size. E.g. when maximum EMA is 6 and step size equals 2, EMAs 2/4/6 are used.')
+    parser.add_argument('-o', '--outfile', default=dir+'/output.csv', help='Specify output file for results. Default: %(default)s')
+    parser.add_argument('-m', '--maxema', default=100, help='Specify the maximum EMA. Default: 100')
+    parser.add_argument('-n', '--steps', default=10, help='Specify step size. Default: 10. E.g. when maximum EMA is 6 and step size equals 2, EMAs 2/4/6 are used.')
     args = parser.parse_args()
 
     # Variables
     filename = args.file
+    outputfile = args.outfile
     maxema = int(args.maxema)
     steps = int(args.steps) 
     
@@ -42,6 +42,9 @@ def main():
     except:
         print('[!] Error: read_csv(). Please check file permissions and format..')
         exit()
+
+    if(maxema == 100 and steps == 10):
+        print('[i] Default values used for maxema and steps..')
     
     # Calculating EMA's
     print('[i] Calculating EMAs...')
@@ -54,8 +57,6 @@ def main():
     # Start testing
     results = pd.DataFrame(columns=['indicators', 'result'])
     price_changes = []
-    deltas = []
-    delta = 0
    
     with Bar('[i] Processing...') as bar:
         for short_ema in df.columns[5:]:
@@ -63,6 +64,8 @@ def main():
             for long_ema in df.columns[5:]:
                 long_ema_index = int(''.join(filter(str.isdigit, long_ema)))
                 if short_ema_index < long_ema_index:
+                    deltas = []
+                    delta = 0
                     initial_state = 1 if df[short_ema][maxema] > df[long_ema][maxema] else 0
                     # Go through df
                     for i in range(maxema,df.shape[0]):
@@ -74,11 +77,11 @@ def main():
                             price_changes.append(i)
                             if state == 1:
                                 if(len(price_changes)>1):
-                                    delta = round(df['low'][price_changes[-2]] - df['high'][i], 2)
+                                    delta = round(df['low'][price_changes[-2]] - df['high'][i], 6)
                                     deltas.append(delta)
                             else:
                                 if(len(price_changes)>1):
-                                    delta = round(df['low'][i] - df['high'][price_changes[-2]], 2)
+                                    delta = round(df['low'][i] - df['high'][price_changes[-2]], 6)
                                     deltas.append(delta)
                             initial_state = state
                     indicator_name = short_ema + ':' + long_ema
@@ -92,6 +95,8 @@ def main():
         print('')
         print('-------------------------------------------')
         print('')
+        print('Writing Results to out.csv')
+        results.to_csv(output_file, index=False)
         print('[+] Best result: ' + str(round(results.result.max(), 2)))
         print('[+] Best indicator: ' + results['indicators'][results.result.idxmax()])
 
