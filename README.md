7400022b43561b1b18e501f56dc34a438f7a7d0f
manu <manu@mac.local>
2020-04-11 17:10:24 +0200

    fixing..

--- README.md
+++ README.md
@@ -2,19 +2,24 @@
 :information_source: Check out the repository from [terukusu](https://github.com/terukusu/download-tick-from-dukascopy) for downloading data (dukascopy)
 ## Moving Average Convergence Divergence (MACD)
 ### Description
-This is a tool to evaluate the best two EMA's for the MACD strategy.
+This is a tool to evaluate the best two EMA's for the MACD strategy. The result is the sum of all delta values.
 ### Usage
 ```bash
 python3 MACD.py -h
 ```
 
 ```bash
-python3 MACD.py -f sample_data/GBPJPY_20191201_20200327/sub_data.csv -m 500 -n 20 -o out.csv
+python3 MACD.py -f sample_data/GBPJPY_20191201_20200327/sub_data.csv -m 1000 -n 100 -p 4 -g -o out.csv 
 ```
 ### Example output
 ```
 [+] Reading in data..
 [+] Calculating EMAs...
 [+] Calculating best EMAs... |################################| 100/100
+-------------------------------
+[+] Best indicator: EMA500 / EMA1000
+[+] Result: 1.64
+-------------------------------
 [+] Writing results to: out.csv
+[+] Showing plot of results..
 ```
--- moving_average_convergence_divergence/MACD.py
+++ moving_average_convergence_divergence/MACD.py
@@ -1,3 +1,4 @@
+# todo: investigate bug: why ema9000:ema1000 = 0?? but when run with test.py not???
 # -*- encoding: utf-8 -*-
 import pandas as pd
 from datetime import datetime as dt
@@ -5,6 +6,7 @@ import argparse
 import os
 from progress.bar import Bar
 import multiprocessing
+import plotly.express as px
 
 dir = os.getcwd()
 
@@ -14,46 +16,43 @@ def calc_emas(df, steps, maxema):
             col_name = 'EMA' + str(i)
             df[col_name] = df['close'].ewm(span=i, adjust=False).mean()
 
-def calc_best_emas(df, short_ema, maxema, steps):
-    short_ema_index = int(''.join(filter(str.isdigit, short_ema)))
-    for long_ema in df.columns[5:]:
-        long_ema_index = int(''.join(filter(str.isdigit, long_ema)))
-        if short_ema_index < long_ema_index:
-            deltas = []
-            price_changes = []
-            delta = 0
-            initial_state = 1 if df[short_ema][maxema] > df[long_ema][maxema] else 0
-            # Go through df
-            for i in range(maxema,df.shape[0]):
-                if df[short_ema][i] > df[long_ema][i]:
-                    state = 1
-                else:
-                    state = 0
-                if state != initial_state:
-                    price_changes.append(i)
-                    if state == 1:
-                        if(len(price_changes)>1):
-                            delta = round(df['low'][price_changes[-2]] - df['high'][i], 6)
-                            deltas.append(delta)
-                    else:
-                        if(len(price_changes)>1):
-                            delta = round(df['low'][i] - df['high'][price_changes[-2]], 6)
-                            deltas.append(delta)
-                    initial_state = state
-            indicator_name = short_ema + ':' + long_ema
-            indicator_result = sum(deltas)
-            return_string = indicator_name + ', ' + str(indicator_result)
-            return return_string
+def calc_best_emas(df, short_ema, long_ema):
+    deltas = []
+    price_changes = []
+    delta = 0
+    maxema = int(''.join(filter(str.isdigit, long_ema)))
+    initial_state = 1 if df[short_ema][maxema] > df[long_ema][maxema] else 0
+    # Go through df
+    for i in range(maxema,df.shape[0]):
+        if df[short_ema][i] > df[long_ema][i]:
+            state = 1
+        else:
+            state = 0
+        if state != initial_state:
+            price_changes.append(i)
+            if state == 1:
+                if(len(price_changes)>1):
+                    delta = round(df['low'][price_changes[-2]] - df['high'][i], 6)
+                    deltas.append(delta)
+            else:
+                if(len(price_changes)>1):
+                    delta = round(df['low'][i] - df['high'][price_changes[-2]], 6)
+                    deltas.append(delta)
+            initial_state = state
+    indicator_name = short_ema + ' / ' + long_ema
+    indicator_result = sum(deltas)
+    return {indicator_name: indicator_result}
 
 def main():
 
     # Argument parser
     parser = argparse.ArgumentParser(description='Get most profitable moving averages for MACD strategy.') 
     parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='Input csv file with no header and the following columns: date, open, high, low, close')
-    parser.add_argument('-o', '--outfile', default=dir+'/output.csv', help='Specify output file for results. Default: %(default)s')
+    parser.add_argument('-o', '--outfile', default=None, help='Specify output file to save results in CSV format.')
     parser.add_argument('-m', '--maxema', default=100, help='Specify the maximum EMA. Default: 100')
     parser.add_argument('-n', '--steps', default=10, help='Specify step size. Default: 10. E.g. when maximum EMA is 6 and step size equals 2, EMAs 2/4/6 are used.')
     parser.add_argument('-p', '--processes', default=4, help='Specify number of cores used for multiprocessing. Default: 4.')
+    parser.add_argument('-g', '--graph', action='store_true', help='Specify this flag without a value to get a plot of results.')
     args = parser.parse_args()
 
     # Variables
@@ -62,9 +61,9 @@ def main():
     maxema = int(args.maxema)
     steps = int(args.steps) 
     n_processes = int(args.processes)
-    print(n_processes)
     pool = multiprocessing.Pool(processes=n_processes)
-    results = []
+    plot = args.graph
+    results = {}
     
     # Checking arguments
     if(filename == None):
@@ -84,30 +83,66 @@ def main():
 
     # Checking if default values were used
     if(maxema == 100 and steps == 10):
-        print('[i] Default values used for maxema and steps..')
+        print('[i] Default values used for max_ema and steps..')
     
     # Calculating EMA's
     print('[+] Calculating EMAs...')
     calc_emas(df, steps, maxema)
-
     # Start processing
+    current_progress = 0
     with Bar('[+] Calculating best EMAs...') as bar:
-        progress = (1/len(df.columns[5:])) * 100
+        progress = 1 / sum([x-1 for x in list(range(1,len(df.columns[5:])+1))]) * 100
         for short_ema in df.columns[5:]:
-            process = pool.apply_async(calc_best_emas, args=(df, short_ema, maxema, steps,), callback=results.append)
-            process.wait()
-            bar.next(progress)
+            short_ema_index = int(''.join(filter(str.isdigit, short_ema)))
+            for long_ema in df.columns[5:]:
+                long_ema_index = int(''.join(filter(str.isdigit, long_ema)))
+                if short_ema_index < long_ema_index:
+                    process = pool.apply_async(calc_best_emas, args=(df, short_ema, long_ema,), callback=results.update)
+                    process.wait()
+                    bar.next(progress)
+                    current_progress += progress
+                    if(current_progress > 99):
+                        bar.next(100-current_progress)
 
-    results = list(filter(None, results))
-    results = '\n'.join(results)
-    #print('[+] Best result: ' + str(round(results.result.max(), 2)))
-    #print('[+] Best indicator: ' + results['indicators'][results.result.idxmax()])
-    print('[+] Writing results to: ' + str(outfile))
-    outfile = open(outfile, 'w')
-    outfile.write('Indicator, Result\n')
-    outfile.write(results)
-    outfile.write('\n')
-    outfile.close()
+    results = pd.DataFrame(results.items(), columns=['indicator', 'result'])
+    print('-------------------------------')
+    print('[+] Best indicator: ' + results['indicator'][results.result.idxmax()])
+    print('[+] Result: ' + str(round(results.result.max(), 2)))
+    print('-------------------------------')
+    if outfile != None:
+        print('[+] Writing results to: ' + str(outfile))
+        results.to_csv(outfile)
+    if plot==True:
+        print('[+] Showing plot of results..')
+        # Creating plot
+        fig = px.line(results, x='indicator', y='result')
+        fig.add_shape(
+            # Line Horizontal
+                type="line",
+                x0=0,
+                y0=results.result.max(),
+                x1=results.result.idxmax(),
+                y1=results.result.max(),
+                line=dict(
+                    color='red',
+                    width=1,
+                    dash="dashdot",
+                )
+        )
+        fig.add_shape(
+            # Line Vertical 
+                type="line",
+                x0=results.result.idxmax(),
+                y0=results.result.min(),
+                x1=results.result.idxmax(),
+                y1=results.result.max(),
+                line=dict(
+                    color='red',
+                    width=1,
+                    dash="dashdot",
+                )
+        )
+        fig.show()
 
 if __name__=='__main__':
     main()
