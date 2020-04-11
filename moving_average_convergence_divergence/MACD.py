ce2057cee1bf3841c98a84aafd5ef0d226d094f5
manu <manu@mac.local>
2020-04-11 09:27:43 +0200

    improved code by multiprocessing, updated README

--- moving_average_convergence_divergence/MACD.py
+++ moving_average_convergence_divergence/MACD.py
@@ -1,12 +1,49 @@
 # -*- encoding: utf-8 -*-
 import pandas as pd
 from datetime import datetime as dt
-import argparse
+import argparse 
 import os
 from progress.bar import Bar
+import multiprocessing
 
 dir = os.getcwd()
 
+def calc_emas(df, steps, maxema):
+    for i in range(steps, maxema+1):
+        if(i % steps ==0):
+            col_name = 'EMA' + str(i)
+            df[col_name] = df['close'].ewm(span=i, adjust=False).mean()
+
+def calc_best_emas(df, short_ema, maxema, steps):
+    short_ema_index = int(''.join(filter(str.isdigit, short_ema)))
+    for long_ema in df.columns[5:]:
+        long_ema_index = int(''.join(filter(str.isdigit, long_ema)))
+        if short_ema_index < long_ema_index:
+            deltas = []
+            price_changes = []
+            delta = 0
+            initial_state = 1 if df[short_ema][maxema] > df[long_ema][maxema] else 0
+            # Go through df
+            for i in range(maxema,df.shape[0]):
+                if df[short_ema][i] > df[long_ema][i]:
+                    state = 1
+                else:
+                    state = 0
+                if state != initial_state:
+                    price_changes.append(i)
+                    if state == 1:
+                        if(len(price_changes)>1):
+                            delta = round(df['low'][price_changes[-2]] - df['high'][i], 6)
+                            deltas.append(delta)
+                    else:
+                        if(len(price_changes)>1):
+                            delta = round(df['low'][i] - df['high'][price_changes[-2]], 6)
+                            deltas.append(delta)
+                    initial_state = state
+            indicator_name = short_ema + ':' + long_ema
+            indicator_result = sum(deltas)
+            return_string = indicator_name + ', ' + str(indicator_result)
+            return return_string
 
 def main():
 
@@ -23,82 +60,51 @@ def main():
     outfile = args.outfile
     maxema = int(args.maxema)
     steps = int(args.steps) 
+    pool = multiprocessing.Pool(4)
+    results = []
     
     # Checking arguments
     if(filename == None):
         print('[?] Use python3 MACD.py --help for more information..')
         exit()
-    
     if(maxema % steps != 0):
         print('[!] Error: Max.EMA % steps =! 0..')
         exit()
 
     # Read data
-    print('')
-    print('[i] Reading in data..')
-    print('')
+    print('[+] Reading in data..')
     try:
         df = pd.read_csv(args.file, names=['date', 'open', 'high', 'low', 'close'], index_col=False)
     except:
         print('[!] Error: read_csv(). Please check file permissions and format..')
         exit()
 
+    # Checking if default values were used
     if(maxema == 100 and steps == 10):
         print('[i] Default values used for maxema and steps..')
     
     # Calculating EMA's
-    print('[i] Calculating EMAs...')
-    print('')
-    for i in range(steps, maxema+1):
-        if(i % steps ==0):
-            col_name = 'EMA' + str(i)
-            df[col_name] = df['close'].ewm(span=i, adjust=False).mean()
+    print('[+] Calculating EMAs...')
+    calc_emas(df, steps, maxema)
 
-    # Start testing
-    results = pd.DataFrame(columns=['indicators', 'result'])
-    price_changes = []
-   
-    with Bar('[i] Processing...') as bar:
+    # Start processing
+    with Bar('[+] Calculating best EMAs...') as bar:
+        progress = (1/len(df.columns[5:])) * 100
         for short_ema in df.columns[5:]:
-            short_ema_index = int(''.join(filter(str.isdigit, short_ema)))
-            for long_ema in df.columns[5:]:
-                long_ema_index = int(''.join(filter(str.isdigit, long_ema)))
-                if short_ema_index < long_ema_index:
-                    deltas = []
-                    delta = 0
-                    initial_state = 1 if df[short_ema][maxema] > df[long_ema][maxema] else 0
-                    # Go through df
-                    for i in range(maxema,df.shape[0]):
-                        if df[short_ema][i] > df[long_ema][i]:
-                            state = 1
-                        else:
-                            state = 0
-                        if state != initial_state:
-                            price_changes.append(i)
-                            if state == 1:
-                                if(len(price_changes)>1):
-                                    delta = round(df['low'][price_changes[-2]] - df['high'][i], 6)
-                                    deltas.append(delta)
-                            else:
-                                if(len(price_changes)>1):
-                                    delta = round(df['low'][i] - df['high'][price_changes[-2]], 6)
-                                    deltas.append(delta)
-                            initial_state = state
-                    indicator_name = short_ema + ':' + long_ema
-                    indicator_result = sum(deltas)
-                    results = results.append({'indicators': indicator_name, 'result': indicator_result}, ignore_index=True)
-            progress = round((1/len(df.columns[5:]))*100, 2)
-            if short_ema == max(df.columns):
-                progress = progress + 1 # to resolve rounding issues
+            process = pool.apply_async(calc_best_emas, args=(df, short_ema, maxema, steps,), callback=results.append)
+            process.wait()
             bar.next(progress)
-        print('')
-        print('')
-        print('-------------------------------------------')
-        print('')
-        print('[+] Writing Results to ' + str(outfile))
-        results.to_csv(outfile, index=False)
-        print('[+] Best result: ' + str(round(results.result.max(), 2)))
-        print('[+] Best indicator: ' + results['indicators'][results.result.idxmax()])
+
+    results = list(filter(None, results))
+    results = '\n'.join(results)
+    #print('[+] Best result: ' + str(round(results.result.max(), 2)))
+    #print('[+] Best indicator: ' + results['indicators'][results.result.idxmax()])
+    print('[+] Writing results to: ' + str(outfile))
+    outfile = open(outfile, 'w')
+    outfile.write('Indicator, Result\n')
+    outfile.write(results)
+    outfile.write('\n')
+    outfile.close()
 
 if __name__=='__main__':
     main()
